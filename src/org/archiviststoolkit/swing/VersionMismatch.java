12d6c612c880eed09ba2a96e714a37ab0ba5d700
Nathan Stevens <nathan@linux-sg7f.site>
2011-10-03 19:52:11 -0400

    "Final" code update for issue 165

--- src/org/archiviststoolkit/swing/VersionMismatch.java
+++ src/org/archiviststoolkit/swing/VersionMismatch.java
@@ -1,5 +1,5 @@
 /**
- * Archivists' Toolkit(TM) Copyright © 2005-2007 Regents of the University of California, New York University, & Five Colleges, Inc.
+ * Archivists' Toolkit(TM) Copyright ï¿½ 2005-2007 Regents of the University of California, New York University, & Five Colleges, Inc.
  * All rights reserved.
  *
  * This software is free. You can redistribute it and / or modify it under the terms of the Educational Community License (ECL)
@@ -50,65 +50,68 @@ public class VersionMismatch extends JDialog {
 
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
-		// Generated using JFormDesigner non-commercial license
-		panel1 = new JPanel();
-		message = new JLabel();
-		downLoadNewVersion = new JLabel();
-		contactAdmin = new JLabel();
-		CellConstraints cc = new CellConstraints();
+        // Generated using JFormDesigner non-commercial license
+        panel1 = new JPanel();
+        message = new JLabel();
+        downLoadNewVersion = new JLabel();
+        contactAdmin = new JLabel();
+        CellConstraints cc = new CellConstraints();
 
-		//======== this ========
-		setBackground(new Color(200, 205, 232));
-		setForeground(Color.white);
-		setModal(true);
-		Container contentPane = getContentPane();
-		contentPane.setLayout(new FormLayout(
-			"default:grow",
-			"fill:default:grow"));
+        //======== this ========
+        setBackground(new Color(200, 205, 232));
+        setForeground(Color.white);
+        setModal(true);
+        Container contentPane = getContentPane();
+        contentPane.setLayout(new FormLayout(
+            "default:grow",
+            "fill:default:grow"));
 
-		//======== panel1 ========
-		{
-			panel1.setBorder(Borders.DLU4_BORDER);
-			panel1.setBackground(new Color(200, 205, 232));
-			panel1.setLayout(new FormLayout(
-				ColumnSpec.decodeSpecs("default:grow"),
-				new RowSpec[] {
-					FormFactory.DEFAULT_ROWSPEC,
-					FormFactory.UNRELATED_GAP_ROWSPEC,
-					FormFactory.DEFAULT_ROWSPEC,
-					FormFactory.LINE_GAP_ROWSPEC,
-					FormFactory.DEFAULT_ROWSPEC
-				}));
+        //======== panel1 ========
+        {
+            panel1.setBorder(Borders.DLU4_BORDER);
+            panel1.setBackground(new Color(200, 205, 232));
+            panel1.setLayout(new FormLayout(
+                ColumnSpec.decodeSpecs("default:grow"),
+                new RowSpec[] {
+                    FormFactory.DEFAULT_ROWSPEC,
+                    FormFactory.UNRELATED_GAP_ROWSPEC,
+                    FormFactory.DEFAULT_ROWSPEC,
+                    FormFactory.LINE_GAP_ROWSPEC,
+                    FormFactory.DEFAULT_ROWSPEC
+                }));
 
-			//---- message ----
-			message.setText("sdfsf");
-			message.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
-			panel1.add(message, cc.xy(1, 1));
+            //---- message ----
+            message.setText("sdfsf");
+            message.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+            panel1.add(message, cc.xy(1, 1));
 
-			//---- downLoadNewVersion ----
-			downLoadNewVersion.setText("<html><u>Download Latest Version</u></html>");
-			downLoadNewVersion.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
-			downLoadNewVersion.addMouseListener(new MouseAdapter() {
-				public void mouseClicked(MouseEvent e) {
-					downLoadNewVersionMouseClicked(e);
-				}
-			});
-			panel1.add(downLoadNewVersion, cc.xywh(1, 3, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
+            //---- downLoadNewVersion ----
+            downLoadNewVersion.setText("<html><u>Download Latest Version</u></html>");
+            downLoadNewVersion.setFont(new Font("Trebuchet MS", Font.PLAIN, 13));
+            downLoadNewVersion.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    downLoadNewVersionMouseClicked(e);
+                }
+            });
+            panel1.add(downLoadNewVersion, cc.xywh(1, 3, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
 
-			//---- contactAdmin ----
-			contactAdmin.setText("Please contact your AT administrator to upgrade the database.");
-			panel1.add(contactAdmin, cc.xywh(1, 5, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
-		}
-		contentPane.add(panel1, cc.xy(1, 1));
+            //---- contactAdmin ----
+            contactAdmin.setText("Please contact your administrator to upgrade the database.");
+            panel1.add(contactAdmin, cc.xywh(1, 5, 1, 1, CellConstraints.CENTER, CellConstraints.DEFAULT));
+        }
+        contentPane.add(panel1, cc.xy(1, 1));
+        pack();
+        setLocationRelativeTo(getOwner());
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
 	}
 
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
-	// Generated using JFormDesigner non-commercial license
-	private JPanel panel1;
-	private JLabel message;
-	private JLabel downLoadNewVersion;
-	private JLabel contactAdmin;
+    // Generated using JFormDesigner non-commercial license
+    private JPanel panel1;
+    private JLabel message;
+    private JLabel downLoadNewVersion;
+    private JLabel contactAdmin;
 	// JFormDesigner - End of variables declaration  //GEN-END:variables
 
 	public final void showDialog() {
