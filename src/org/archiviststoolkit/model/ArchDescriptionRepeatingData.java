c6effd0494d5b2229eabb285838e5e52f3b08a6b
Nathan Stevens <nathan@quanta2.bobst.nyu.edu>
2012-05-29 09:52:29 -0400

    Brings code in sync with AT 2.0 update 14 beta

--- src/org/archiviststoolkit/model/ArchDescriptionRepeatingData.java
+++ src/org/archiviststoolkit/model/ArchDescriptionRepeatingData.java
@@ -1,5 +1,5 @@
 /**
- * Archivists' Toolkit(TM) Copyright © 2005-2007 Regents of the University of California, New York University, & Five Colleges, Inc.
+ * Archivists' Toolkit(TM) Copyright ï¿½ 2005-2007 Regents of the University of California, New York University, & Five Colleges, Inc.
  * All rights reserved.
  *
  * This software is free. You can redistribute it and / or modify it under the terms of the Educational Community License (ECL)
@@ -101,7 +101,7 @@ public abstract class ArchDescriptionRepeatingData extends SequencedObject imple
 		this.repeatingDataType = repeatingDataType;
 	}
 
-	private void linkArchDescription(ArchDescription archDescription) {
+	public void linkArchDescription(ArchDescription archDescription) {
 		if (archDescription instanceof Resources) {
 			this.resource = (Resources) archDescription;
 		} else if (archDescription instanceof ResourcesComponents) {
